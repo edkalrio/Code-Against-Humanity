08ef77b1074442fc123158d8486df0791368cca1
Eduardo Calvo <edkalrio@lunaticgeek.com>
2016-12-21 14:01:58 +0100

    Update cah.sh

--- cah.sh
+++ cah.sh
@@ -3,18 +3,9 @@
 #Code Against Humanity is a Fortune-like program based on Cards Against Humanity.
 
 #Copyright (C) 2015 edkalrio
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU Affero General Public License as
-#published by the Free Software Foundation, either version 3 of the
-#License, or (at your option) any later version.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#GNU Affero General Public License for more details.
-
-#You should have received a copy of the GNU Affero General Public License
-#along with this program. If not, see http://www.gnu.org/licenses/.
+#Copyright (C) 2015 Cards Against Humanity
+#Copyright (C) 2016 Chris Hallberg
+#Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
 
 #Why is it written in bash? --because I'm sysadmin, not a programmer.
 
@@ -26,7 +17,8 @@
 read RND_B1 < <(shuf bcards.txt)
 read RND_W1 < <(shuf wcards.txt)
 read RND_W2 < <(shuf wcards.txt)
+read RND_W3 < <(shuf wcards.txt)
 
 #Strings are matched and piped to the output.
 
-echo -e "$RND_B1" | sed -e "s/\_/$RND_W1/; s/\_/$RND_W2/"
+echo -e "$RND_B1" | sed -e "s/\_/$RND_W1/; s/\_/$RND_W2/; s/\_/$RND_W3/"
